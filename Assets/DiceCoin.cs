4de029d6ec17261c0ee48b3ae45978ccf2777751
Fraser Killip <fraserk@olympic.co.nz>
2018-01-25 00:43:39 +1300

    Stuff

---
+++ Assets/DiceCoin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
+
+public class DiceCoin : MonoBehaviour, IPointerClickHandler
+{
+	public static int up = 0;
+	public static int down = 0;
+	public static int side = 0;
+	public static int count = 0;
+	
+	public bool moving = true;
+
+	public float size = 1;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Reset();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rb = GetComponent<Rigidbody>();
+		if (rb.velocity.magnitude < 0.001 && moving)
+		{
+			var dotUp = Vector3.Dot(transform.up, Vector3.up);
+			
+			var mr = GetComponent<MeshRenderer>();
+			if (dotUp < -0.9)
+			{
+				mr.material.color = Color.blue;
+				down++;
+			}
+			else if (dotUp > 0.9)
+			{
+				mr.material.color = Color.red;
+				up++;
+			}
+			else
+			{
+				mr.material.color = Color.yellow;
+				side++;
+			}
+
+			count++;
+
+			moving = false;
+		}
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log(eventData.button);
+	}
+
+	public void Reset()
+	{
+		var rb = GetComponent<Rigidbody>();
+		moving = true;
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+		var mr = GetComponent<MeshRenderer>();
+		mr.material.color = Color.white;
+
+		
+		
+		transform.rotation = Random.rotationUniform;
+		//transform.rotation = Quaternion.Euler(90, 90, 0);
+		rb.AddForceAtPosition(Random.insideUnitSphere * Random.Range(8, 20), Random.insideUnitSphere);
+		rb.velocity = Random.onUnitSphere * Random.Range(1, 10);
+		
+		//transform.localScale = new Vector3(1, (float) (0.5f / (2 * Math.Sqrt(2))), 1);
+		transform.localScale = new Vector3(1, (float) (0.5f / (size)), 1);
+		var range = 10;
+		transform.position = transform.parent.position + new Vector3(Random.Range(-range, range), 0, Random.Range(-range, range));
+
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Result
+{
+	public int Count { get; set; }
+	public int Up { get; set; }
+	public int Down { get; set; }
+	public int Side { get; set; }
+}
+public class Spawner : MonoBehaviour
+{
+	public int InstanceCount;
+	public Rigidbody Prefab;
+
+	public UnityEngine.UI.Text SizeLabel;
+	public UnityEngine.UI.Text RatioLabel;
+	public UnityEngine.UI.Slider Slider;
+	public UnityEngine.UI.Button Button;
+	public GameObject Canvas;
+	public GameObject BarPrefab;
+
+	public static float Size = 1;
+	
+	public List<Rigidbody> coins = new List<Rigidbody>();
+	public List<Result> results = new List<Result>();
+	public List<GameObject> bars = new List<GameObject>();
+	
+	// Use this for initialization
+	void Start () {
+		// prefill results
+
+		var resultTotal = 20;
+		for (int i = 0; i < resultTotal; i++)
+		{
+			results.Add(new Result());
+		}
+		
+		for (var i = 0; i < InstanceCount; i++)
+		{
+			var range = 10;
+			var position = transform.position + new Vector3(Random.Range(-range, range), 0, Random.Range(-range, range));
+			var diceCoin = Instantiate(Prefab, position, transform.rotation);
+			diceCoin.transform.parent = transform;
+			diceCoin.GetComponent<DiceCoin>().size = Slider.value;
+			coins.Add(diceCoin);
+		}
+
+		for (int i = 0; i < resultTotal; i++)
+		{
+			var bar = Instantiate(BarPrefab, Vector3.zero, Quaternion.identity);
+			bar.transform.SetParent(Canvas.transform);
+			bar.transform.position = new Vector3(10 * i, 0, 0);
+			
+			
+			SetBarForResult(bar, new Result());
+			bars.Add(bar);
+		}
+		
+		
+		
+		Button.onClick.AddListener(() =>
+		{
+			coins.ForEach(c =>
+			{
+				var diceCoin = c.GetComponent<DiceCoin>();
+				diceCoin.Reset();
+				diceCoin.size = Slider.value;
+			});
+
+			DiceCoin.count = 0;
+			DiceCoin.down = 0;
+			DiceCoin.up = 0;
+			DiceCoin.side = 0;
+
+			RatioLabel.text = "Simulating...";
+			results.Clear();
+			for (int i = 0; i < resultTotal; i++)
+			{
+				results.Add(new Result());
+				SetBarForResult(bars[i], results[i]);
+			}
+		});
+	}
+
+	private static void SetBarForResult(GameObject bar, Result result)
+	{
+		if (result.Count == 0) result.Count += 1;
+		
+		var heightUp = 100 * (result.Up / (float) result.Count);
+		var heightDown = 100 * (result.Down / (float) result.Count);
+		var heightSide = 100 * (result.Side / (float) result.Count);
+		
+		
+		var child0 = bar.transform.GetChild(0);
+		child0.transform.localPosition = new Vector3(0, 0, 0);
+		child0.GetComponent<RectTransform>().sizeDelta = new Vector2(10, heightUp);
+		child0.GetComponent<Image>().color = Color.blue;
+
+		var child1 = bar.transform.GetChild(1);
+		child1.transform.localPosition = new Vector3(0, heightUp, 0);
+		child1.GetComponent<RectTransform>().sizeDelta = new Vector2(10, heightDown);
+		child1.GetComponent<Image>().color = Color.red;
+
+		var child2 = bar.transform.GetChild(2);
+		child2.transform.localPosition = new Vector3(0, heightUp + heightDown, 0);
+		child2.GetComponent<RectTransform>().sizeDelta = new Vector2(10, heightSide);
+		child2.GetComponent<Image>().color = Color.yellow;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		SizeLabel.text = string.Format("Size: {0:0.00}", Slider.value);
+		Size = Slider.value;
+
+		var movingCoins = coins.Where(c => c.GetComponent<DiceCoin>().moving).ToList().Count;
+		
+		if (DiceCoin.count > 0)
+		{
+			RatioLabel.text = string.Format("{0} - Up: {1:0.00}, Down: {2:0.00}, Side: {3:0.00} -- Moving coins: {4}/{5}", DiceCoin.count,
+				DiceCoin.up / (float)DiceCoin.count, DiceCoin.down / (float)DiceCoin.count, DiceCoin.side / (float)DiceCoin.count, movingCoins, InstanceCount);
+		}
+
+		if (movingCoins == 0)
+		{
+			results.RemoveAt(0);
+			results.Add(new Result
+			{
+				Count = DiceCoin.count,
+				Down = DiceCoin.down,
+				Side = DiceCoin.side,
+				Up = DiceCoin.up
+			});
+
+			for (var i = 0; i < bars.Count; i++)
+			{
+				SetBarForResult(bars[i], results[i]);
+			}
+			
+			coins.ForEach(c => c.GetComponent<DiceCoin>().Reset());
+		}
+	}
+}
